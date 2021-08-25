9daac0c0585d447b6893b105e603b220b97185e2
Simon Stranks <sstranks87@gmail.com>
2021-08-25 12:27:59 +0100

    In Progress

--- FrontEndMentor/11_Coming_Soon_Page/script.js
+++ FrontEndMentor/11_Coming_Soon_Page/script.js
@@ -1,33 +1,42 @@
 'use strict'
 
+// Variables
 const btn = document.querySelector('button[type="submit"]');
 const emailInput = document.querySelector('input');
+const errorIcon = document.querySelector('.error-icon');
+const errorMsg = document.querySelector('.error-msg');
 var checkBool = false;
 
+
+// Email Validation
 const validate = (e) => {
   var emailReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   // e.preventDefault()
   if (emailInput.value == '') {
-    alert("None");
-    return;
+    errorIcon.classList.remove('hidden');
+    errorMsg.classList.remove('hidden');
   }
 
   if (emailInput.value.match(emailReg)) {
     alert("Email Valid");
   } else {
-    alert("Email Invalid");
     btn.style.background = "linear-gradient(135deg, hsla(0, 80%, 86%, 0.5), hsla(0, 74%, 74%, 0.5))";
+    errorIcon.classList.remove('hidden');
+    errorMsg.classList.remove('hidden');
     checkBool = true;
   }
 }
 
+// Reset Styles
 const reset = (e) => {
   if (checkBool == true) {
     btn.style.background = "linear-gradient(135deg, hsl(0, 80%, 86%), hsl(0, 74%, 74%))";
+    errorIcon.classList.add('hidden');
+    errorMsg.classList.add('hidden');
   }
 }
 
-
+// Event Listeners
 btn.addEventListener('click', validate);
 emailInput.addEventListener('input', reset);
 
