9c9c9297bb8593c094c84a8330a03ea04f6b83e2
Simon Stranks <sstranks87@gmail.com>
2021-08-30 17:45:10 +0100

    In Progress

---
+++ FrontEndMentor/16_Blogr_Landing_Page/script.js
@@ -0,0 +1,9 @@
+'use strict'
+
+// Selectors
+const btn_product = document.getElementById('product');
+const btn_company = document.getElementById('company');
+const btn_connect = document.getElementById('connect');
+const sub_1 = document.querySelector('.sub-1');
+const sub_2 = document.querySelector('.sub-2');
+const sub_3 = document.querySelector('.sub-3');
