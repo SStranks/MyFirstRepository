2b36200286f90760bf7f3be000013249a135cd3e
Simon Stranks <sstranks87@gmail.com>
2021-08-31 14:45:35 +0100

    Challenge Complete

--- FrontEndMentor/16_Blogr_Landing_Page/script.js
+++ FrontEndMentor/16_Blogr_Landing_Page/script.js
@@ -1,12 +1,6 @@
 'use strict'
 
 // Selectors
-// const btn_product = document.getElementById('product');
-// const btn_company = document.getElementById('company');
-// const btn_connect = document.getElementById('connect');
-// const sub_1 = document.querySelector('.sub-1');
-// const sub_2 = document.querySelector('.sub-2');
-// const sub_3 = document.querySelector('.sub-3');
 const mob_menu = document.getElementById('mob-menu');
 const menu = document.querySelector('.menu-nav');
 const windowResize = window.matchMedia("(max-width: 375px)");
