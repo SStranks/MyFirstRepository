010b79800b8b69e9264f0efd06f469db18bacde1
Simon Stranks <sstranks87@gmail.com>
2021-09-07 00:06:49 +0100

    Challenge Complete

--- FrontEndMentor/21_Fylo_Component/script.js
+++ FrontEndMentor/21_Fylo_Component/script.js
@@ -6,11 +6,15 @@ const track = document.querySelector('.track-inner');
 const gbLeft1 = document.querySelector('#GBnum1');
 const gbLeft2 = document.querySelector('#GBnum2');
 const amount = document.querySelector('.amount');
+const slideContainer = document.querySelector('.slidercontainer');
 
 
 // Update the left-hand colour of the slider track
 const updateSlider = (value) => {
-  track.style.width = `${value}%`;
+  // 4px gap between the scroll outer and inner track, at each end
+  let maxWidth = slideContainer.scrollWidth - 8;
+  track.style.width = (value === 0) ? 0 : `${((maxWidth / 100) * value)}px`;
+  // track.style.width = `${value}%`;
   gbLeft1.textContent = gbLeft2.textContent = `${value * 10}`;
 }
 
