296dd0f2381b98654a6d86dc13d211297ee17b07
Simon Stranks <sstranks87@gmail.com>
2021-11-11 23:59:43 +0000

    CodeWars

---
+++ CodeWars/8 Kyu/GrassHopperTerminalGameMoveFunc.js
@@ -0,0 +1,8 @@
+// In this game, the hero moves from left to right. The player rolls the die and moves the number of spaces indicated by the die two times.
+
+// Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+// My Solution
+function move (position, roll) {
+  return position + (roll * 2);
+}
---
+++ CodeWars/8 Kyu/GrasshopperMessiGoals.js
@@ -0,0 +1,19 @@
+// Use variables to find the sum of the goals Messi scored in 3 competitions
+
+// Competition	      Goals
+// La Liga	          43
+// Champions League	10
+// Copa del Rey	    5
+
+// Create these three variables and store the appropriate values using the table above:
+//   laLigaGoals
+//   championsLeagueGoals
+//   copaDelReyGoals
+// Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+
+// My Solution
+const laLigaGoals = 43;
+const championsLeagueGoals = 10;
+const copaDelReyGoals = 5;
+
+const totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
