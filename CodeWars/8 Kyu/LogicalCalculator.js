07e810f164a913bf0ae97f8d56345cee70fe0fd7
Simon Stranks <sstranks87@gmail.com>
2021-11-15 22:17:29 +0000

    CodeWars

---
+++ CodeWars/8 Kyu/LogicalCalculator.js
@@ -0,0 +1,41 @@
+// Your Task
+// Given an array of Boolean values and a logical operator, return a Boolean result based on sequentially applying the operator to the values in the array.
+
+// Examples
+// booleans = [True, True, False], operator = "AND"
+// True AND True -> True
+// True AND False -> False
+// return False
+// booleans = [True, True, False], operator = "OR"
+// True OR True -> True
+// True OR False -> True
+// return True
+// booleans = [True, True, False], operator = "XOR"
+// True XOR True -> False
+// False XOR False -> False
+// return False
+// Input
+// an array of Boolean values (1 <= array_length <= 50)
+// a string specifying a logical operator: "AND", "OR", "XOR"
+// Output
+// A Boolean value (True or False).
+
+
+// My Solution
+function logicalCalc(array, op){
+  if (op === "AND") return !!array.reduce((acc, cur) => acc && cur);
+  if (op === "OR") return !!array.reduce((acc, cur) => acc || cur);
+  if (op === "XOR") return !!array.reduce((acc, cur) => acc ^ cur);
+}
+
+// Other Solutions
+var ops = {
+  'AND': (a, b) => a && b,
+  'OR': (a, b) =>  a || b,
+  'XOR': (a, b) => a !== b
+}
+
+
+function logicalCalc(array, op){
+  return array.reduce(ops[op]);
+}
