72939cc319b980ef5477cecf5ea5cb49978987a0
Simon Stranks <sstranks87@gmail.com>
2021-11-18 11:06:38 +0000

    CodeWars

---
+++ CodeWars/8 Kyu/CountOddNumbersBelowN.js
@@ -0,0 +1,16 @@
+// Given a number n, return the number of positive odd numbers below n, EASY!
+
+// oddCount(7) //=> 3, i.e [1, 3, 5]
+// oddCount(15) //=> 7, i.e [1, 3, 5, 7, 9, 11, 13]
+// Expect large Inputs!
+
+
+// My Solution
+function oddCount(n){
+  return Math.round((n - 1) / 2);
+}
+
+// Other Solution
+function oddCount(n){
+  return Math.floor(n/2);
+}
---
+++ CodeWars/8 Kyu/ReturnToSanity.js
@@ -0,0 +1,7 @@
+// This function should return an object, but it's not doing what's intended. What's wrong?
+
+// My Solution
+function mystery() {
+  let results = {sanity: 'Hello'};
+  return results;
+}
