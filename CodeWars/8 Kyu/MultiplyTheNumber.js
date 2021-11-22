ea1a762913df5b077ffc40ce60dfb8b5e7039c93
Simon Stranks <sstranks87@gmail.com>
2021-11-22 19:40:29 +0000

    CodeWars

---
+++ CodeWars/8 Kyu/MultiplyTheNumber.js
@@ -0,0 +1,13 @@
+// Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
+
+// multiply(3)==15 // 3 * 5¹
+// multiply(10)==250 // 10 * 5²
+// multiply(200)==25000 // 200 * 5³
+// multiply(0)==0 // 0 * 5¹
+// multiply(-3)==-15 // -3 * 5¹
+
+
+// My Solution
+function multiply(number){
+  return number * (5 ** Math.abs(number).toString().length);
+}
