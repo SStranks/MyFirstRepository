5f6cd969104fd4fb08e62c50fb8578c3a9c554b9
Simon Stranks <sstranks87@gmail.com>
2021-11-25 19:00:19 +0000

    CodeWars

---
+++ CodeWars/8 Kyu/LexicalThis.js
@@ -0,0 +1,29 @@
+// Lexical this
+// Complete the Person object, by completing the FillFriends function to fill the _friends Array for the person object.
+// Output
+// Return the person object with the Name, Friends and FillFriends function filled out which will fill the input into the Friends property.
+// Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+
+// My Solution
+var Person = function(){
+  var person = {
+    _name: "Leroy",
+    _friends: [],
+    fillFriends(f) {f.forEach((el) => this._friends.push(el))}
+  }
+  return person;
+}
+
+
+// Other Solutions
+var Person = function(){
+  var person = {
+    _name: "Leroy",
+    _friends: [],
+    fillFriends(f){
+      this._friends.push(...f);
+    }
+  }
+  return person;
+}
---
+++ CodeWars/8 Kyu/Power.js
@@ -0,0 +1,17 @@
+// The goal is to create a function 'numberToPower(number, power)' that "raises" the number up to power (ie multiplies number by itself power times).
+
+// Examples
+// numberToPower(3,2)  // -> 9 ( = 3 * 3 )
+// numberToPower(2,3)  // -> 8 ( = 2 * 2 * 2 )
+// numberToPower(10,6) // -> 1000000
+// Note: Math.pow and some other Math functions like eval() and ** are disabled.
+
+
+// My Solution
+function numberToPower(number, power){
+  let total = 1;
+  for (let i = 1; i <= power; i++) { 
+    total = total * number;
+  }
+  return total;
+}
