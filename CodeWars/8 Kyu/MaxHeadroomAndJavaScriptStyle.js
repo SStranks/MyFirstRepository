86116ad4eafb3f696de59afb7bc63964c389dd9f
Simon Stranks <sstranks87@gmail.com>
2021-11-26 12:40:21 +0000

    CodeWars

---
+++ CodeWars/8 Kyu/MaxHeadroomAndJavaScriptStyle.js
@@ -0,0 +1,12 @@
+// Shouldn't the two functions getMax1 and getMax2 be equivalent and return the same value? Can you spot a problem and fix it? Can you learn something about JavaScript style in this kata?
+
+
+// My Solution
+function getMax1() {
+  var max = {name: 'Max Headroom'}
+  return max;
+}
+
+function getMax2() {
+  return {name: 'Max Headroom'};
+}
---
+++ CodeWars/8 Kyu/SemiOptional.js
@@ -0,0 +1,16 @@
+// We have implemented a function wrap(value) that takes a value of arbitrary type and wraps it in a new Javascript Object setting the 'value' key on the new Object to the passed-in value.
+
+// So, for example, if we execute the following code:
+
+// var wrappedObj = wrap("MyWrappedString"); 
+//  // wrappedObject should be  {"value":"MyWrappedString"}
+// We would then expect the following statement to be true:
+
+// wrappedObj.value === "MyWrappedString"
+// Unfortunately, the code is not working as designed. Please fix the code so that it behaves as specified.
+
+
+// My Solution
+function wrap(value) {
+  return {value};
+}
