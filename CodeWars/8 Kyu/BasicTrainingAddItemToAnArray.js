511f1d40348c29b0e09a8a16edb3dd6445993fb1
Simon Stranks <sstranks87@gmail.com>
2021-11-26 16:33:35 +0000

    CodeWars

---
+++ CodeWars/8 Kyu/BasicTrainingAddItemToAnArray.js
@@ -0,0 +1,11 @@
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+
+// var websites = [];
+
+
+// My Solutions
+// add the value "codewars" to the already defined websites array
+websites.push("codewars");
