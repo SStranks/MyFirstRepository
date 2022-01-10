45f66942c01af116d7fc1ee06038bf6a2df261aa
Simon Stranks <sstranks87@gmail.com>
2022-01-10 21:22:04 +0000

    Renamed and refactored

---
+++ ThreeJS/Sketch.js
@@ -0,0 +1,163 @@
+import { getTime, cloneObject } from './utils'
+import TextCharMesh from './TextCharMesh';
+
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+import Stats from 'three/examples/jsm/libs/stats.module';
+
+import * as dat from 'dat.gui';
+import gsap from 'gsap';
+import { VideoTexture } from 'three';
+
+
+export default class Sketch {
+  constructor(options){
+    this.charSpace = options.charSpace || 5;
+    this.textMeshes = {};
+    this.time3DGroup = new THREE.Group();
+    this._Initialize();
+    this._AddObjects();
+    this._Debug();
+    this._Render();
+  }
+
+  _Initialize() {
+    // Renderer
+    this._renderer = new THREE.WebGLRenderer({ antialias: true });
+    this._renderer.setPixelRatio(devicePixelRatio);
+    this._renderer.setSize(innerWidth, innerHeight);
+
+    // Inject WebGL canvas
+    document.body.appendChild(this._renderer.domElement);
+
+    // Resize
+    window.addEventListener('resize', () => {
+      this._OnWindowResize();
+    }, false);
+
+    // Camera
+    const cOpt = { fov: 75, aspect: innerWidth / innerHeight, near: 0.1, far: 1000 };
+    this._camera = new THREE.PerspectiveCamera(cOpt.fov, cOpt.aspect, cOpt.near, cOpt.far);
+    this._camera.position.set(0, 10, 30);
+
+    // Scene
+    this._scene = new THREE.Scene();
+
+    // Lights
+    const light = new THREE.DirectionalLight(0xffffff, 1); // Color, brightness
+    light.position.set(0, 1, 1);
+    this._scene.add(light);
+
+    // Controls
+    this._controls = new OrbitControls(this._camera, this._renderer.domElement);
+    this._controls.update();
+    
+    // Helpers
+    // const lightHelper = new THREE.DirectionalLightHelper(light);
+    // const gridHelper = new THREE.GridHelper(200, 50);
+  }
+
+  _AddObjects() {
+    const loader = new FontLoader();
+    loader.load('helvetiker_regular.typeface.json', function(font) {
+      const textChars = "0123456789:";
+      for (let i = 0; i < textChars.length; i++){
+        const options = { font, x : 0 + (i * 10), y : 0, z : -10}
+        const mesh = new TextCharMesh(options, textChars[i]);
+        const meshWidth = TextCharMesh.width
+        if (meshWidth > this.charSpace) this.charSpace = meshWidth;
+        mesh.textMesh.position.set(0 + (i * 10), 0, -10);
+        this.textMeshes[textChars[i]] = mesh
+        this._scene.add(mesh.textMesh);
+      }
+      this._Generate3DTime();
+    }.bind(this));
+  }
+
+  _Debug() {
+    // this._gui = new dat.GUI();
+    // const world = {}
+    // Stats: FPS
+    this._statsFPS = new Stats();
+    this._statsFPS.domElement.style.cssText = "position:absolute;top:3px;left:3px;";
+    this._statsFPS.showPanel(0); // 0: fps,
+    // Stats: Memory
+    this._statsMemory = new Stats();
+    this._statsMemory.showPanel(2); //2: mb, 1: ms, 3+: custom
+    this._statsMemory.domElement.style.cssText = "position:absolute;top:3px;left:84px;";
+    // Stats: Add to DOM
+    document.body.appendChild(this._statsFPS.dom);
+    document.body.appendChild(this._statsMemory.dom);
+  }
+
+  _Generate3DTime() {
+    const currentTime = getTime();
+    for (let i = 0; i < currentTime.length; i++) {
+      const numMesh = cloneObject(currentTime[i], this._scene)
+      numMesh.position.set((this.charSpace * i), 0, 0);
+      // Adjust alignment of ":"
+      if (currentTime[i] === ":") {
+        const width = this.textMeshes[currentTime[i]].width;
+        const moveXpos = (this.charSpace / 2) - (width / 2)
+        numMesh.position.x += moveXpos;
+        // TEMP: Showing Arrows
+        const arrowHelpers = this.textMeshes[currentTime[i]].arrowHelpers;
+        arrowHelpers.forEach((el) => {
+          el.position.x += moveXpos + this.charSpace * 5
+          this._scene.add(el)
+        });
+      }
+      numMesh.name = `clone_${i}_${currentTime[i]}`
+      numMesh.visible = true;
+      this.time3DGroup.add(numMesh);
+    }
+    this._scene.add(this.time3DGroup);
+  }
+  
+  _Refresh3DTime(currentTime) {
+    for (let i = 0; i < this.time3DGroup.children.length; i++) {
+      const char = this.time3DGroup.children[i].name
+      if (char[char.length - 1] !== currentTime[i]) {
+        const oldNumber = this.time3DGroup.children[i];
+        const newNumber = cloneObject(currentTime[i], this._scene);
+        this.time3DGroup.children[i] = newNumber;
+        oldNumber.parent.remove(oldNumber)
+        newNumber.position.set((this.charSpace * i), 0, 0);
+        newNumber.name = `clone_${i}_${currentTime[i]}`
+        newNumber.visible = true;
+        this._scene.add(newNumber);
+      }
+    }
+  }
+
+  _OnWindowResize() {
+    this._camera.aspect = window.innerWidth / window.innerHeight;
+    this._renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  _UpdateTime() {
+    this.previousTime = null;
+    this.currentTime = getTime();
+    if (this.currentTime !== this.previousTime) {
+      this.previousTime = this.currentTime;
+      this._Refresh3DTime(this.currentTime);
+    }
+  }
+
+  _Render() {
+    // time += 0.01;
+    this._controls.update();
+    this._statsFPS.update();
+    this._statsMemory.update();
+    this._UpdateTime();
+    // particlesArr.forEach((particle, i) =>{
+    //   updateParticle(particle, time);
+    //   posArray.set([particle[0], particle[1], particle[2]], i * 3)
+    // });
+    // particlesMesh.geometry.attributes.position.needsUpdate = true;
+
+    requestAnimationFrame(this._Render.bind(this))
+    this._renderer.render(this._scene, this._camera)
+  }
+}
