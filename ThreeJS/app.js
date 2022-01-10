65f8680e050aa05b19a214ddd867890b1dba5c1d
Simon Stranks <sstranks87@gmail.com>
2022-01-10 21:20:17 +0000

    Created App

---
+++ ThreeJS/app.js
@@ -0,0 +1,21 @@
+'use strict'
+
+import './style.css'
+import Sketch from './Sketch.js';
+
+new Sketch({
+  // dom: document.getElementById("container"),
+  // config: false,
+  // url: myimage,
+  // mouseover: "#mouseover",
+  // settings:{
+    // number: 5000,
+    // trails: 0.1,
+    // size: 0.7,
+    // gravity:0.24,
+    // gravityDifference:0.08,
+    // randomness: 0.5,
+    // sideScale: 4,
+    // speedScale: 8
+  // }
+});
