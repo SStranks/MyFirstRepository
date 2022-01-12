84be31bbb3fd7c8afa6ac3e1b41572f47ac86c1d
Simon Stranks <sstranks87@gmail.com>
2022-01-12 10:59:15 +0000

    CodeWars

---
+++ CodeWars/7 Kyu/HellsKitchen.js
@@ -0,0 +1,14 @@
+// Gordon Ramsay shouts. He shouts and swears. There may be something wrong with him.
+
+// Anyway, you will be given a string of four words. Your job is to turn them in to Gordon language.
+
+// Rules:
+
+// Obviously the words should be Caps, Every word should end with '!!!!', Any letter 'a' or 'A' should become '@', Any other vowel should become '*'.
+
+
+// My Solution
+function gordon(a){
+  return a.replace(/(\w)+/gi, "$&!!!!").replace(/[a]/gi, "@").replace(/[eiou]/gi, "*").toUpperCase()
+}
+
