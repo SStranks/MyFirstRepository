dfcaace4ee8334036c64b20b29132496a7afb697
Simon Stranks <sstranks87@gmail.com>
2022-02-16 16:46:09 +0000

    Initial Setup

---
+++ FrontEndMentor/27_Time_Tracking_Dashboard/timetrackdash/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserCard from './UserCard';
+
+function App() {
+  return (
+    <div className="App">
+      <UserCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ FrontEndMentor/27_Time_Tracking_Dashboard/timetrackdash/src/components/UserCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserCard = (props) => {
+  return (
+    <div>
+      <div>Test</div>
+      <div>Test</div>
+    </div>
+  );
+};
+
+export default UserCard;
