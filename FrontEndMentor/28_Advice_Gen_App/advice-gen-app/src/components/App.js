896a24d2263ad535d261ad7189fea2f5f156103b
Simon Stranks <sstranks87@gmail.com>
2022-02-24 18:49:38 +0000

    API Connected

--- FrontEndMentor/28_Advice_Gen_App/advice-gen-app/src/components/App.js
+++ FrontEndMentor/28_Advice_Gen_App/advice-gen-app/src/components/App.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from './Card';
 
 function App() {
-  return <Card />;
+  const [advice, setAdvice] = useState({});
+
+  const fetchAdvice = async () => {
+    try {
+      const getData = await fetch('https://api.adviceslip.com/advice');
+      const data = await getData.json();
+      const jsonData = {
+        id: data.slip.id,
+        quote: data.slip.advice,
+      };
+      setAdvice(jsonData);
+    } catch {
+      throw new Error('Trouble reaching AdviceAPI');
+    }
+  };
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  const clickHandler = () => {
+    fetchAdvice();
+  };
+
+  return <Card title={advice.id} quote={advice.quote} click={clickHandler} />;
 }
 
 export default App;
--- FrontEndMentor/28_Advice_Gen_App/advice-gen-app/src/components/Card.js
+++ FrontEndMentor/28_Advice_Gen_App/advice-gen-app/src/components/Card.js
@@ -2,17 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Card = (props) => {
-  const { title, quote } = props;
+  const { title, quote, click } = props;
   return (
     <div className="card">
-      <h2 className="title">{title}</h2>
+      <h2 className="title">ADVICE #{title}</h2>
       <p className="quote">{quote}</p>
       <img
         className="divider"
         src="assets/pattern-divider-desktop.svg"
         alt=""
       />
-      <div className="luminous">
+      <div
+        className="luminous"
+        onClick={click}
+        onKeyDown={click}
+        role="button"
+        tabIndex="0"
+      >
         <img src="assets/icon-dice.svg" alt="dice" />
       </div>
     </div>
@@ -22,11 +28,13 @@ const Card = (props) => {
 Card.propTypes = {
   title: PropTypes.string,
   quote: PropTypes.string,
+  click: PropTypes.func,
 };
 
 Card.defaultProps = {
   title: 'ADVICE #',
   quote: 'Silence is golden.',
+  click: null,
 };
 
 export default Card;
