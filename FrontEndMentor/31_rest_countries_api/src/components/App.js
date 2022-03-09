75fb700805a8dcf1dcfe565fe3aaa1dc65ca6d57
Simon Stranks <sstranks87@gmail.com>
2022-03-09 12:47:09 +0000

    Added Fetch

--- FrontEndMentor/31_rest_countries_api/src/components/App.js
+++ FrontEndMentor/31_rest_countries_api/src/components/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './Header';
 import Main from './Main';
 
 function App() {
+  const countriesData = [];
+  const getCountries = async () => {
+    try {
+      const getData = await fetch('https://restcountries.com/v2/all');
+      const parseData = await getData.json();
+      countriesData.push(...parseData);
+      localStorage.setItem('countries', JSON.stringify(countriesData));
+    } catch (err) {
+      console.log('Error retreiving data: ', err);
+    }
+  };
+
+  useEffect(() => {
+    const getData = localStorage.getItem('countries');
+    return getData ? countriesData.push(JSON.parse(getData)) : getCountries();
+  }, []);
+
   return (
     <>
       <Header />
-      <Main />
+      {/* <Main /> */}
     </>
   );
 }
