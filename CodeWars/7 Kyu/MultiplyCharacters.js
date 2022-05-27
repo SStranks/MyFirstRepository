4a7ed3533c2c7ed2bdb5511d5770e5e301a10c32
Simon Stranks <sstranks87@gmail.com>
2022-05-27 10:34:56 +0100

    CodeWars

---
+++ CodeWars/7 Kyu/MultiplyCharacters.js
@@ -0,0 +1,12 @@
+// Here we have a function that help us spam our hearty laughter. But is not working! I need you to find out why...
+
+// Expected results:
+
+// spam(1);  // hue
+// spam(6);  // huehuehuehuehuehue
+// spam(14); // huehuehuehuehuehuehuehuehuehuehuehuehuehue
+
+// My Solution
+function spam(number) {
+  return 'hue'.repeat(number);
+}
