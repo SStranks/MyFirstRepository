73874a616814b7eba37301510dd9cf2f6786e176
Simon Stranks <sstranks87@gmail.com>
2022-07-01 11:32:33 +0100

    CodeWars

--- CodeWars/7 Kyu/PowersOfI.js
+++ CodeWars/7 Kyu/PowersOfI.js
@@ -3,5 +3,18 @@
 // Your Task
 // Complete the function pofi that returns iii to the power of a given non-negative integer in its simplest form, as a string (answer may contain iii).
 
-
 // My Solution
+function pofi(n) {
+  const dict = {
+    0: '1',
+    1: 'i',
+    2: '-1',
+    3: '-i',
+  };
+  return dict[n - Math.floor(n / 4) * 4];
+}
+
+// Other Solutions
+function pofi(n) {
+  return ['1', 'i', '-1', '-i'][n % 4];
+}
