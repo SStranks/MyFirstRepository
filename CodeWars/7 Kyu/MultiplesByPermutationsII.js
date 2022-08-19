d648ea3230255816dbbf7be9abac0db7f206a876
Simon Stranks <sstranks87@gmail.com>
2022-08-19 16:24:28 +0100

    CodeWars

--- CodeWars/7 Kyu/MultiplesByPermutationsII.js
+++ CodeWars/7 Kyu/MultiplesByPermutationsII.js
@@ -29,8 +29,11 @@
 
 // My Solution
 function findLowestInt(k) {
-  let n = 1;
+  let n = 3;
   const x = k + 1;
   const string = (n) => n.toString().split``.sort().join``;
-  while (1) if (string(n * k) === string(n++ * x)) return --n;
+  while (true) {
+    if (string(n * k) === string(n * x)) return n;
+    n += 3;
+  }
 }
