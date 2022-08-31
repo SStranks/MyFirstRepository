cad392d359ed397337f246d8bc90758fbda41de5
Simon Stranks <sstranks87@gmail.com>
2022-08-31 17:02:44 +0100

    Add Image Support

--- FrontEndMentor/32_dev_jobs_webapp/Frontend/webpack.config.js
+++ FrontEndMentor/32_dev_jobs_webapp/Frontend/webpack.config.js
@@ -68,6 +68,10 @@ module.exports = {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
