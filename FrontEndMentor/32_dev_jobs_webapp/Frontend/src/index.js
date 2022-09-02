f4efe06edbc5b401e720228e62496661b0feba9b
Simon Stranks <sstranks87@gmail.com>
2022-09-02 15:06:04 +0100

    React 17->18 Syntax

--- FrontEndMentor/32_dev_jobs_webapp/Frontend/src/index.js
+++ FrontEndMentor/32_dev_jobs_webapp/Frontend/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './components/App';
 import './assets/styles/global-imports.scss';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+// ReactDOM.render(<App />, document.querySelector('#root'));
+const container = document.querySelector('#root');
+const root = createRoot(container);
+root.render(<App />);
