ebb974ab68be6503b3feaea25191aeae356a523b
Simon Stranks <sstranks87@gmail.com>
2022-09-03 17:15:45 +0100

    Styles WIP

--- FrontEndMentor/32_dev_jobs_webapp/Frontend/src/components/card/Card.js
+++ FrontEndMentor/32_dev_jobs_webapp/Frontend/src/components/card/Card.js
@@ -3,15 +3,17 @@ import PropTypes from 'prop-types';
 import styles from './_Card.module.scss';
 
 function Card(props) {
-  const { logo, details, title, company, location } = props;
+  const { logo, postedAt, contract, position, company, location } = props;
 
   return (
     <div className={styles.card}>
       <div>
         <img src={logo} alt="company logo" />
       </div>
-      <p>{details}</p>
-      <h3>{title}</h3>
+      <p>
+        {postedAt}.{contract}
+      </p>
+      <h3>{position}</h3>
       <p>{company}</p>
       <h4>{location}</h4>
     </div>
@@ -20,16 +22,18 @@ function Card(props) {
 
 Card.propTypes = {
   logo: PropTypes.string,
-  details: PropTypes.string,
-  title: PropTypes.string,
+  postedAt: PropTypes.string,
+  contract: PropTypes.string,
+  position: PropTypes.string,
   company: PropTypes.string,
   location: PropTypes.string,
 };
 
 Card.defaultProps = {
   logo: PropTypes.string,
-  details: PropTypes.string,
-  title: PropTypes.string,
+  postedAt: PropTypes.string,
+  contract: PropTypes.string,
+  position: PropTypes.string,
   company: PropTypes.string,
   location: PropTypes.string,
 };
--- FrontEndMentor/32_dev_jobs_webapp/Frontend/src/components/main/Main.js
+++ FrontEndMentor/32_dev_jobs_webapp/Frontend/src/components/main/Main.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import Button from '../custom/Button';
+import Card from '../card/Card';
 import styles from './_Main.module.scss';
 
+// Temporary Data
+import jsonData from '../../data.json';
+
 function Main() {
-  return <div className={styles.grid} />;
+  const jobCards = jsonData.map((obj) => (
+    <Card
+      key={obj.id}
+      company={obj.company}
+      logo={`./src/assets/svg/${obj.logo.slice(9)}`}
+      logoBackground={obj.logoBackground}
+      position={obj.position}
+      postedAt={obj.postedAt}
+      contract={obj.contract}
+      location={obj.location}
+    />
+  ));
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.grid}>{jobCards}</div>
+      <Button text="Load More" />
+    </div>
+  );
 }
 
 export default Main;
