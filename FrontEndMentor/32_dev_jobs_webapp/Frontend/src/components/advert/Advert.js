d3e15684cc2518ca3b6dfaddc7dd097e8433c8a0
Simon Stranks <sstranks87@gmail.com>
2022-09-05 18:19:11 +0100

    Styles WIP

--- FrontEndMentor/32_dev_jobs_webapp/Frontend/src/components/advert/Advert.js
+++ FrontEndMentor/32_dev_jobs_webapp/Frontend/src/components/advert/Advert.js
@@ -1,13 +1,108 @@
 import React from 'react';
-// import styles from './_Advert.module.scss';
+import PropTypes from 'prop-types';
+import styles from './_Advert.module.scss';
+import Button from '../custom/Button';
 
-function Advert() {
+function Advert(props) {
+  const {
+    company,
+    website,
+    logo,
+    logoBackground,
+    postedAt,
+    contract,
+    position,
+    location,
+    description,
+    requirements,
+    role,
+  } = props;
+
+  const requirementList = requirements.items.map((el) => (
+    <li key={el.id}>{el}</li>
+  ));
+  const roleList = role.items.map((el) => <li key={el.id}>{el}</li>);
+  console.log(logo.slice(2));
+
   return (
-    <>
-      <div />
-      <div />
-    </>
+    <div className={styles.layout}>
+      <div className={styles.banner}>
+        <div style={{ 'background-color': logoBackground }}>
+          <img src={logo.slice(2)} alt="company logo" />
+        </div>
+        <div>
+          <div>
+            <h2>{company}</h2>
+            <p>{website}</p>
+          </div>
+          <Button text="Company Site" />
+        </div>
+      </div>
+      <div className={styles.main}>
+        <div>
+          <div>
+            <p>
+              {postedAt}
+              <span>.</span>
+              {contract}
+            </p>
+            <h1>{position}</h1>
+            <h4>{location}</h4>
+          </div>
+          <Button text="Apply Now" />
+        </div>
+        <div>
+          <p>{description}</p>
+          <h3>Requirements</h3>
+          <p>{requirements.content}</p>
+          <ul>{requirementList}</ul>
+          <h3>What You Will Do</h3>
+          <p>{role.content}</p>
+          <ol>{roleList}</ol>
+        </div>
+      </div>
+    </div>
   );
 }
+
+Advert.propTypes = {
+  logo: PropTypes.string,
+  logoBackground: PropTypes.string,
+  company: PropTypes.string,
+  website: PropTypes.string,
+  postedAt: PropTypes.string,
+  contract: PropTypes.string,
+  position: PropTypes.string,
+  location: PropTypes.string,
+  description: PropTypes.string,
+  requirements: PropTypes.shape({
+    content: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.string),
+  }),
+  role: PropTypes.shape({
+    content: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.string),
+  }),
+};
+
+Advert.defaultProps = {
+  logo: PropTypes.string,
+  logoBackground: PropTypes.string,
+  company: PropTypes.string,
+  website: PropTypes.string,
+  postedAt: PropTypes.string,
+  contract: PropTypes.string,
+  position: PropTypes.string,
+  location: PropTypes.string,
+  description: PropTypes.string,
+  requirements: PropTypes.shape({
+    content: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.string),
+  }),
+  role: PropTypes.shape({
+    content: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.string),
+  }),
+};
 
 export default Advert;
