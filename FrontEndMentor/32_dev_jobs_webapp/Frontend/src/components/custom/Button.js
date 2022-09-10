d58f900fb0d33269995567a61eda8f9e4bc7f9b7
Simon Stranks <sstranks87@gmail.com>
2022-09-10 18:11:37 +0100

    Added disabled prop

--- FrontEndMentor/32_dev_jobs_webapp/Frontend/src/components/custom/Button.js
+++ FrontEndMentor/32_dev_jobs_webapp/Frontend/src/components/custom/Button.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 import styles from './_Button.module.scss';
 
 function Button(props) {
-  const { onClick, text } = props;
+  const { onClick, text, disabled } = props;
 
   return (
     <div className={styles['button-custom']}>
-      <button type="button" onClick={onClick}>
+      <button type="button" onClick={onClick} disabled={disabled}>
         {text}
       </button>
     </div>
@@ -17,11 +17,13 @@ function Button(props) {
 Button.propTypes = {
   text: PropTypes.string,
   onClick: PropTypes.func,
+  disabled: PropTypes.bool,
 };
 
 Button.defaultProps = {
   text: null,
   onClick: null,
+  disabled: null,
 };
 
 export default Button;
