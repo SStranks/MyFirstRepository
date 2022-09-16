ef41bd0cab0049f10a9c25252090c281a7e418ab
Simon Stranks <sstranks87@gmail.com>
2022-09-16 23:58:58 +0100

    Removed comment

--- FrontEndMentor/32_dev_jobs_webapp/Frontend/webpack.common.js
+++ FrontEndMentor/32_dev_jobs_webapp/Frontend/webpack.common.js
@@ -4,11 +4,6 @@ module.exports = {
   entry: './src/index.js',
   target: 'web',
   resolve: {
-    /** "extensions"
-     * If multiple files share the same name but have different extensions, webpack will
-     * resolve the one with the extension listed first in the array and skip the rest.
-     * This is what enables users to leave off the extension when importing
-     */
     extensions: ['.js', '.jsx', '.json'],
   },
   module: {
