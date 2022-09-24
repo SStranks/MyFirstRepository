1cc81f1699ea4b885518b982ef182b5e5d1a96ec
Simon Stranks <sstranks87@gmail.com>
2022-09-24 15:39:02 +0100

    init

---
+++ FrontEndMentor/33_project_feedback_app/frontend/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as SortBar } from './sort-bar/SortBar.jsx';
+export { default as Tags } from './tags/Tags.jsx';
+export { default as Roadmap } from './roadmap/Roadmap.jsx';
+export { default as SuggestionsList } from './suggestions-list/SuggestionsList.jsx';
