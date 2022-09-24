9a342bf4c089919a7de07d35286cb2090e938c46
Simon Stranks <sstranks87@gmail.com>
2022-09-24 11:02:00 +0100

    test: init

---
+++ FrontEndMentor/33_project_feedback_app/frontend/src/components/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import '@testing-library/jest-dom';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+test('');
