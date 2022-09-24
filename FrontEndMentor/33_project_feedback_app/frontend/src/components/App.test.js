f439a2f856e3285bb6a57184c78e971eace39814
Simon Stranks <sstranks87@gmail.com>
2022-09-24 13:34:18 +0100

    test: basic structure

--- FrontEndMentor/33_project_feedback_app/frontend/src/components/App.test.js
+++ FrontEndMentor/33_project_feedback_app/frontend/src/components/App.test.js
@@ -5,4 +5,32 @@ import '@testing-library/jest-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-test('');
+test('full app rendering', async () => {
+  render(<App />, { wrapper: BrowserRouter });
+  const user = userEvent.setup();
+
+  // verify page content for default route
+  // expect(screen.getByText(/you are home/i)).toBeInTheDocument()
+});
+
+test('navigating to roadmap page', async () => {
+  render(<App />, { wrapper: BrowserRouter });
+  const user = userEvent.setup();
+
+  // verify page content for expected route after navigating
+  // await user.click(screen.getByText(/about/i));
+  // expect(screen.getByText(/you are on the about page/i)).toBeInTheDocument();
+});
+
+test('navigating to unknown page', () => {
+  const badRoute = '/some/bad/route';
+
+  render(
+    <MemoryRouter initialEntries={[badRoute]}>
+      <App />
+    </MemoryRouter>
+  );
+
+  // verify navigation to "no match" route
+  // expect(screen.getByText(/no match/i)).toBeInTheDocument()
+});
