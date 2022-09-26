6e5a281b7e00f077b6e824115c540cab0b8c39b4
Simon Stranks <sstranks87@gmail.com>
2022-09-26 11:07:24 +0100

    init

---
+++ FrontEndMentor/33_project_feedback_app/frontend/src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './assets/sass/global-imports.scss';
+import App from './components/App';
+
+const container = document.querySelector('#root');
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
