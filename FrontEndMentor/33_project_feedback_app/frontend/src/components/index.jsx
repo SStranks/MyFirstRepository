0912f7cc8094f0fb27b7e2065db6aa3f99e953bf
Simon Stranks <sstranks87@gmail.com>
2022-09-26 19:27:06 +0100

    del

--- FrontEndMentor/33_project_feedback_app/frontend/src/components/index.jsx
+++
@@ -1,8 +0,0 @@
-export { default as AsideRoadmap } from './aside-roadmap/AsideRoadmap';
-export { default as AsideTags } from './aside-tags/AsideTags';
-export { default as Comments } from './comments/Comments';
-export { default as Form } from './form/Form';
-export { default as Header } from './header/Header';
-export { default as Suggestion } from './suggestion/Suggestion';
-export { default as SuggestionsList } from './suggestions-list/SuggestionsList';
-export { default as UtilityBar } from './utility-bar/UtilityBar';
