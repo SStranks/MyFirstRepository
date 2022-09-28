65a997373b7a318b6b881f2787fe6d635c2f09a0
Simon Stranks <sstranks87@gmail.com>
2022-09-28 17:10:40 +0100

    chore: styles WIP

--- FrontEndMentor/33_project_feedback_app/frontend/src/components/utility-bar/suggestions/UtilityBar.jsx
+++ FrontEndMentor/33_project_feedback_app/frontend/src/components/utility-bar/suggestions/UtilityBar.jsx
@@ -1,17 +1,32 @@
 import IconArrowDown from '../../../assets/svg/shared/icon-arrow-down.svg';
 import IconSuggestion from '../../../assets/svg/shared/icon-suggestions.svg';
+import Dropdown from '../../custom/dropdown/Dropdown';
 import styles from './_UtilityBar.module.scss';
 
 function UtilityBar() {
+  // Temporary Dev
+  const numSuggestions = 6;
+
+  const dropdownList = [
+    'Most Upvotes',
+    'Least Upvotes',
+    'Most Comments',
+    'Least Comments',
+  ];
+
   return (
     <div className={styles.bar}>
       <div className={styles.bar__suggestions}>
         <img src={IconSuggestion} alt="" />
-        <h3>6 Suggestions</h3>
-        <div className={styles.bar__suggestions__sort}>
+        <h3>{numSuggestions} Suggestions</h3>
+        <button
+          type="button"
+          className={styles.bar__sort}
+          onClick={clickHandlerDropdown}>
           <p>Sort by : Most Upvotes</p>
           <img src={IconArrowDown} alt="" />
-        </div>
+          <Dropdown listItems={dropdownList} />
+        </button>
       </div>
       <button type="button" className={styles.btnPlaceholder}>
         + Add Feedback
