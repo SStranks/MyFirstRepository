2fb92fcd867311e78611d1af0c56aa492f48512f
Simon Stranks <sstranks87@gmail.com>
2022-09-29 17:24:11 +0100

    init

---
+++ FrontEndMentor/33_project_feedback_app/frontend/src/components/custom/textarea/Textarea.jsx
@@ -0,0 +1,58 @@
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import styles from './_Textarea.module.scss';
+
+function Textarea(props) {
+  const { name, id, cols, rows, innerRef, formError, setFormError } = props;
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    if (formError) setError(true);
+    innerRef.current.focus();
+  }, [formError, innerRef]);
+
+  const textInputHandler = (e) => {
+    if (error && e.target.value.length > 0) {
+      setError(false);
+      setFormError(false);
+    }
+  };
+
+  return (
+    <div className={`${error ? styles['textarea--error'] : ''}`}>
+      <textarea
+        className={`${styles.textarea} ${
+          error ? styles['textarea--error'] : ''
+        }`}
+        onChange={textInputHandler}
+        ref={innerRef}
+        name={name}
+        id={id}
+        cols={cols}
+        rows={rows}
+      />
+    </div>
+  );
+}
+
+Textarea.propTypes = {
+  name: PropTypes.string,
+  id: PropTypes.string,
+  cols: PropTypes.number,
+  rows: PropTypes.number,
+  formError: PropTypes.bool,
+  setFormError: PropTypes.func,
+  innerRef: PropTypes.shape(),
+};
+
+Textarea.defaultProps = {
+  name: PropTypes.string,
+  id: PropTypes.string,
+  cols: PropTypes.number,
+  rows: PropTypes.number,
+  formError: PropTypes.bool,
+  setFormError: PropTypes.func,
+  innerRef: undefined,
+};
+
+export default Textarea;
