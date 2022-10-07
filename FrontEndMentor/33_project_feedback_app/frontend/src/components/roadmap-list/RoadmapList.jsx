42b5973ccef91d64a9cc6966355e07c870f3cdf0
Simon Stranks <sstranks87@gmail.com>
2022-10-07 11:55:12 +0100

    chore: styles WIP

--- FrontEndMentor/33_project_feedback_app/frontend/src/components/roadmap-list/RoadmapList.jsx
+++ FrontEndMentor/33_project_feedback_app/frontend/src/components/roadmap-list/RoadmapList.jsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from 'react';
 import Roadmap from '../roadmap/Roadmap';
 import styles from './_RoadmapList.module.scss';
 
@@ -5,7 +6,30 @@ import styles from './_RoadmapList.module.scss';
 import JSONData from '../../data/data.json';
 
 function RoadmapList() {
+  const [btnMobileFilter, setBtnMobileFilter] = useState({
+    planned: true,
+    'in-progress': false,
+    live: false,
+  });
+
+  const btnMobileFilterClickHandler = (e) => {
+    setBtnMobileFilter({
+      planned: false,
+      'in-progress': false,
+      live: false,
+      [e.target.value]: true,
+    });
+    // const btnActive = document.body.querySelector(
+    //   `button[name = "${e.target.value}"]`
+    // );
+    // btnActive.classList.add(styles.active);
+    // console.log(btnActive);
+  };
+
   // Temprary Dev
+  const num1 = 2;
+  const num2 = 3;
+  const num3 = 1;
   const numPlanned = 2;
   const numProgress = 3;
   const numLive = 1;
@@ -27,6 +51,45 @@ function RoadmapList() {
 
   return (
     <div className={styles.grid}>
+      <div className={styles['grid__mobile-nav']}>
+        <button
+          className={styles['grid__mobile-nav__btn1']}
+          type="button"
+          onClick={btnMobileFilterClickHandler}
+          value="planned"
+          name="planned">
+          Planned ({num1})
+        </button>
+        <button
+          className={styles['grid__mobile-nav__btn2']}
+          type="button"
+          onClick={btnMobileFilterClickHandler}
+          value="in-progress"
+          name="in-progress">
+          In-Progress ({num2})
+        </button>
+        <button
+          className={styles['grid__mobile-nav__btn3']}
+          type="button"
+          onClick={btnMobileFilterClickHandler}
+          value="live"
+          name="live">
+          Live ({num3})
+        </button>
+        <div
+          className={`${styles['grid__mobile-nav__activebar']} ${
+            btnMobileFilter.planned
+              ? styles.col1
+              : btnMobileFilter['in-progress']
+              ? styles.col2
+              : styles.col3
+          }`}
+        />
+      </div>
+      <div className={styles['grid__mobile-nav__title']}>
+        <h3>{`Planned (${numPlanned})`}</h3>
+        <p>Ideas prioritized for research</p>
+      </div>
       <div className={styles.grid__title}>
         <h3>{`Planned (${numPlanned})`}</h3>
         <p>Ideas prioritized for research</p>
--- FrontEndMentor/33_project_feedback_app/frontend/src/components/utility-bar/roadmap/UtilityBar.jsx
+++ FrontEndMentor/33_project_feedback_app/frontend/src/components/utility-bar/roadmap/UtilityBar.jsx
@@ -1,31 +1,9 @@
-import { useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import IconArrowLeft from '../../../assets/svg/shared/icon-arrow-left.svg';
 import Button from '../../custom/button/Button';
 import styles from './_UtilityBar.module.scss';
 
 function UtilityBar() {
-  const [btnMobileFilter, setBtnMobileFilter] = useState({
-    planned: true,
-    'in-progress': false,
-    live: false,
-  });
-  const btnMobileFilterActive = useRef();
-
-  // Temporary Dev
-  const num1 = 2;
-  const num2 = 3;
-  const num3 = 1;
-
-  const btnMobileFilterClickHandler = (e) => {
-    setBtnMobileFilter({
-      planned: false,
-      'in-progress': false,
-      live: false,
-      [e.target.value]: true,
-    });
-  };
-
   return (
     <div className={styles.bar}>
       <div className={styles.bar__nav}>
@@ -42,39 +20,6 @@ function UtilityBar() {
           text="+ Add Feedback"
           disabled={false}
           classList={['bg-magenta']}
-        />
-      </div>
-      <div className={styles['bar__mobile-nav']}>
-        <button
-          className={styles['bar__mobile-nav__btn1']}
-          type="button"
-          onClick={btnMobileFilterClickHandler}
-          value="planned">
-          Planned ({num1})
-        </button>
-        <button
-          className={styles['bar__mobile-nav__btn2']}
-          type="button"
-          onClick={btnMobileFilterClickHandler}
-          value="in-progress">
-          In-Progress ({num2})
-        </button>
-        <button
-          className={styles['bar__mobile-nav__btn3']}
-          type="button"
-          onClick={btnMobileFilterClickHandler}
-          value="live">
-          Live ({num3})
-        </button>
-        <div
-          className={`${styles['bar__mobile-nav__activebar']} ${
-            btnMobileFilter.planned
-              ? styles.col1
-              : btnMobileFilter['in-progress']
-              ? styles.col2
-              : styles.col3
-          }`}
-          ref={btnMobileFilterActive}
         />
       </div>
     </div>
