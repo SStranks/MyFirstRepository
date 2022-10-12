588bde643b1e3cc11cf7a4e6ec693d0709c3bdbf
Simon Stranks <sstranks87@gmail.com>
2022-10-12 16:12:41 +0100

    init

---
+++ FrontEndMentor/34_invoice_app/frontend/webpack.common.js
@@ -0,0 +1,47 @@
+import ESLintPlugin from 'eslint-webpack-plugin';
+// import CopyPlugin from 'copy-webpack-plugin';
+
+export default {
+  entry: './src/index.tsx',
+  target: 'web',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'ts-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: 'html-loader',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ESLintPlugin({
+      extensions: ['js', 'jsx', 'ts', 'tsx'],
+    }),
+    // new CopyPlugin({
+    //   patterns: [
+    //     {
+    //       from: 'logos/',
+    //       to: 'assets/logos/',
+    //       context: 'src/assets/svg/',
+    //     },
+    //   ],
+    // }),
+  ],
+};
