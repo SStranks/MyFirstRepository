5beb6d85b31c84e24c9bcd7c1169a8407c830a81
Simon Stranks <sstranks87@gmail.com>
2022-10-20 21:21:20 +0100

    chore: styles wip

--- FrontEndMentor/34_invoice_app/frontend/src/components/invoices/invoice-list/InvoiceList.tsx
+++ FrontEndMentor/34_invoice_app/frontend/src/components/invoices/invoice-list/InvoiceList.tsx
@@ -1,23 +1,43 @@
-import Invoice from '#Components/invoices/invoice/Invoice';
+// import Invoice from '#Components/invoices/invoice/Invoice';
+import IllustrationEmpty from '#Svg/illustration-empty.svg';
 // TEMP DEV:  // Data
-import JSONData from '#Data/data.json';
+// import JSONData from '#Data/data.json';
 
 import styles from './InvoiceList.module.scss';
 
 function InvoiceList(): JSX.Element {
   // TEMP DEV:  // Data
-  const invoices = JSONData.map((el) => (
-    <Invoice
-      key={el.id}
-      invoiceId={el.id}
-      paymentDue={el.paymentDue}
-      clientName={el.clientName}
-      total={el.total}
-      status={el.status}
-    />
-  ));
+  // const invoices = JSONData.map((el) => (
+  //   <Invoice
+  //     key={el.id}
+  //     invoiceId={el.id}
+  //     paymentDue={el.paymentDue}
+  //     clientName={el.clientName}
+  //     total={el.total}
+  //     status={el.status}
+  //   />
+  // ));
 
-  return <div className={styles.container}>{invoices}</div>;
+  const invoices = undefined;
+
+  const noInvoices = (
+    <>
+      <img src={IllustrationEmpty} alt="" />
+      <h2>There is nothing here</h2>
+      <p>
+        Create an invoice by clicking the <b>New Invoice</b> button and get
+        started
+      </p>
+    </>
+  );
+
+  return (
+    <div className={styles.container}>
+      {(invoices && (
+        <div className={styles.container__invoices}>{invoices}</div>
+      )) || <div className={styles.container__noInvoices}>{noInvoices}</div>}
+    </div>
+  );
 }
 
 export default InvoiceList;
