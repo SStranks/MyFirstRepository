5824d83ac41245661aebb512638e8ab0481f1b98
Simon Stranks <sstranks87@gmail.com>
2022-10-20 12:28:58 +0100

    chore: styles wip

--- FrontEndMentor/34_invoice_app/frontend/src/components/invoices/invoice-list/InvoiceList.tsx
+++ FrontEndMentor/34_invoice_app/frontend/src/components/invoices/invoice-list/InvoiceList.tsx
@@ -1,12 +1,23 @@
 import Invoice from '#Components/invoices/invoice/Invoice';
+// TEMP DEV:  // Data
+import JSONData from '#Data/data.json';
+
 import styles from './InvoiceList.module.scss';
 
 function InvoiceList(): JSX.Element {
-  return (
-    <div className={styles.container}>
-      <Invoice />
-    </div>
-  );
+  // TEMP DEV:  // Data
+  const invoices = JSONData.map((el) => (
+    <Invoice
+      key={el.id}
+      invoiceId={el.id}
+      paymentDue={el.paymentDue}
+      clientName={el.clientName}
+      total={el.total}
+      status={el.status}
+    />
+  ));
+
+  return <div className={styles.container}>{invoices}</div>;
 }
 
 export default InvoiceList;
--- FrontEndMentor/34_invoice_app/frontend/src/components/invoices/invoice/Invoice.tsx
+++ FrontEndMentor/34_invoice_app/frontend/src/components/invoices/invoice/Invoice.tsx
@@ -1,25 +1,28 @@
 import ArrowRight from '#Svg/icon-arrow-right.svg';
 import styles from './Invoice.module.scss';
 
-function Invoice(): JSX.Element {
-  // Temporary Dev
-  const invoiceCode = 'RT3080';
-  const date = '19 Aug 2021';
-  const name = 'Jensen Huang';
-  const amount = '1,800.90';
-  const status = 'Paid';
+type CompProps = {
+  invoiceId: string;
+  paymentDue: string;
+  clientName: string;
+  total: number;
+  status: string;
+};
+
+function Invoice(props: CompProps): JSX.Element {
+  const { invoiceId, paymentDue, clientName, total, status } = props;
 
   return (
     <div className={styles.container}>
       <p className={styles.container__code}>
-        #<p className={styles['container__code--black']}>{invoiceCode}</p>
+        #<p className={styles['container__code--black']}>{invoiceId}</p>
       </p>
-      <p className={styles.container__date}>Due {date}</p>
-      <p className={styles.container__name}>{name}</p>
-      <p className={styles.container__amount}>£ {amount}</p>
+      <p className={styles.container__date}>Due {paymentDue}</p>
+      <p className={styles.container__name}>{clientName}</p>
+      <p className={styles.container__amount}>£ {total.toFixed(2)}</p>
       <div className={styles.container__status}>
         <div className={styles.container__status__bullet} />
-        <p>{status}</p>
+        <p>{`${status[0].toUpperCase()}${status.slice(1)}`}</p>
       </div>
       <div className={styles.container__arrowright}>
         <img src={ArrowRight} alt="" />
