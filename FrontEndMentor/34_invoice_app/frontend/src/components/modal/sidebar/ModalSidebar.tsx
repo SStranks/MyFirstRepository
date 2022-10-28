246e751c953274790d08faf41bb0161d0ef22fea
Simon Stranks <sstranks87@gmail.com>
2022-10-28 16:22:02 +0100

    chore: styles wip

--- FrontEndMentor/34_invoice_app/frontend/src/components/modal/sidebar/ModalSidebar.tsx
+++ FrontEndMentor/34_invoice_app/frontend/src/components/modal/sidebar/ModalSidebar.tsx
@@ -14,108 +14,115 @@ function ModalSidebar(props: SidebarProps): JSX.Element {
   return (
     <div className={styles.container}>
       <div className={styles.container__subcontainer}>
-        <div className={styles.sidebar}>
-          <p className={styles.sidebar__title}>
+        <div className={styles.container__subcontainer__title}>
+          <p>
             {title} <span>#</span>
             {code}
           </p>
-          <div className={styles.sidebar__from}>
-            <p>Bill From</p>
-            <div className={styles.sidebar__from__street}>
-              <p>Street Address</p>
-              <input type="text" />
-            </div>
-            <div className="">
-              <p>City</p>
-              <input type="text" />
-            </div>
-            <div className="">
-              <p>Post Code</p>
-              <input type="text" />
-            </div>
-            <div className="">
-              <p>Country</p>
-              <input type="text" />
-            </div>
-          </div>
-          <div className={styles.sidebar__to}>
-            <p>Bill To</p>
-            <div className={styles.sidebar__to__name}>
-              <p>Client&#39;s Name</p>
-              <input type="text" />
-            </div>
-            <div className={styles.sidebar__to__email}>
-              <p>Client&#39;s Email</p>
-              <input type="text" />
-            </div>
-            <div className={styles.sidebar__to__address}>
-              <p>Street Address</p>
-              <input type="text" />
-            </div>
-            <div className="">
-              <p>City</p>
-              <input type="text" />
-            </div>
-            <div className="">
-              <p>Post Code</p>
-              <input type="text" />
-            </div>
-            <div className="">
-              <p>Country</p>
-              <input type="text" />
-            </div>
-          </div>
-          <div className={styles.sidebar__details}>
-            <div className="">
-              <p>Invoice Date</p>
-              <input type="text" />
-            </div>
-            <div className="">
-              <p>Payment Terms</p>
-              <input type="text" />
-            </div>
-            <div className={styles.sidebar__details__description}>
-              <p>Project Description</p>
-              <input type="text" />
-            </div>
-          </div>
-          <div className={styles.sidebar__itemlist}>
-            <p className={styles.sidebar__itemlist__title}>Item List</p>
-            <div className={styles.sidebar__itemlist__grid}>
-              <p>Item Name</p>
-              <p>Qty.</p>
-              <p>Price</p>
-              <p>Total</p>
-              <input
-                className={styles['sidebar__itemlist__grid--col1']}
-                type="text"
-              />
-              <input
-                className={styles['sidebar__itemlist__grid--col2']}
-                type="text"
-              />
-              <input
-                className={styles['sidebar__itemlist__grid--col3']}
-                type="text"
-              />
-              <p className={styles['sidebar__itemlist__grid--col4']}>£156.00</p>
-              <img
-                className={styles['sidebar__itemlist__grid--col5']}
-                src={IconDelete}
-                alt=""
-              />
-              <div className={styles.sidebar__itemlist__grid__btnAddItem}>
-                <Button
-                  text="+ Add New Item"
-                  color="grey"
-                  // eslint-disable-next-line unicorn/no-useless-undefined
-                  onClick={() => undefined}
-                  value="additem"
-                  disabled={false}
-                />
+        </div>
+        <div className={styles.sidebar}>
+          <form className={styles.sidebar__form} action="">
+            <div className={styles.sidebar__form__from}>
+              <p>Bill From</p>
+              <div className={styles.sidebar__form__from__street}>
+                <p>Street Address</p>
+                <input type="text" />
+              </div>
+              <div className="">
+                <p>City</p>
+                <input type="text" />
+              </div>
+              <div className="">
+                <p>Post Code</p>
+                <input type="text" />
+              </div>
+              <div className="">
+                <p>Country</p>
+                <input type="text" />
               </div>
             </div>
-          </div>
+            <div className={styles.form__to}>
+              <p>Bill To</p>
+              <div className={styles.sidebar__form__to__name}>
+                <p>Client&#39;s Name</p>
+                <input type="text" />
+              </div>
+              <div className={styles.sidebar__form__to__email}>
+                <p>Client&#39;s Email</p>
+                <input type="text" />
+              </div>
+              <div className={styles.sidebar__form__to__address}>
+                <p>Street Address</p>
+                <input type="text" />
+              </div>
+              <div className="">
+                <p>City</p>
+                <input type="text" />
+              </div>
+              <div className="">
+                <p>Post Code</p>
+                <input type="text" />
+              </div>
+              <div className="">
+                <p>Country</p>
+                <input type="text" />
+              </div>
+            </div>
+            <div className={styles.sidebar__form__details}>
+              <div className="">
+                <p>Invoice Date</p>
+                <input type="text" />
+              </div>
+              <div className="">
+                <p>Payment Terms</p>
+                <input type="text" />
+              </div>
+              <div className={styles.sidebar__form__details__description}>
+                <p>Project Description</p>
+                <input type="text" />
+              </div>
+            </div>
+            <div className={styles.sidebar__form__itemlist}>
+              <p className={styles.sidebar__form__itemlist__title}>Item List</p>
+              <div className={styles.sidebar__form__itemlist__grid}>
+                <p>Item Name</p>
+                <p>Qty.</p>
+                <p>Price</p>
+                <p>Total</p>
+                <input
+                  className={styles['sidebar__form__itemlist__grid--col1']}
+                  type="text"
+                />
+                <input
+                  className={styles['sidebar__form__itemlist__grid--col2']}
+                  type="text"
+                />
+                <input
+                  className={styles['sidebar__form__itemlist__grid--col3']}
+                  type="text"
+                />
+                <p className={styles['sidebar__form__itemlist__grid--col4']}>
+                  £156.00
+                </p>
+                <img
+                  className={styles['sidebar__form__itemlist__grid--col5']}
+                  src={IconDelete}
+                  alt=""
+                />
+                <div
+                  className={styles.sidebar__form__itemlist__grid__btnAddItem}>
+                  <Button
+                    text="+ Add New Item"
+                    color="grey"
+                    // eslint-disable-next-line unicorn/no-useless-undefined
+                    onClick={() => undefined}
+                    value="additem"
+                    disabled={false}
+                  />
+                </div>
+              </div>
+            </div>
+          </form>
           <SidebarButtons>
             {title === 'Edit' ? (
               <div className={styles.sidebar__buttons__editInvoice}>
