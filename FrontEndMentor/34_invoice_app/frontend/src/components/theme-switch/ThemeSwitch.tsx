ac086642ff1eee709ed627f1805f3096fc80dd22
Simon Stranks <sstranks87@gmail.com>
2022-10-28 23:46:57 +0100

    chore: styles wip

--- FrontEndMentor/34_invoice_app/frontend/src/components/theme-switch/ThemeSwitch.tsx
+++ FrontEndMentor/34_invoice_app/frontend/src/components/theme-switch/ThemeSwitch.tsx
@@ -2,11 +2,16 @@ import IconMoon from '#Svg/icon-moon.svg';
 // import IconSun from '#Svg/icon-sun.svg';
 import styles from './ThemeSwitch.module.scss';
 
+const themeToggle = () => {
+  const body = document.querySelector('body');
+  body?.classList.toggle('dark-theme');
+};
+
 function ThemeSwitcher(): JSX.Element {
   return (
-    <div className={styles.container}>
+    <button type="button" className={styles.container} onClick={themeToggle}>
       <img src={IconMoon} alt="" />
-    </div>
+    </button>
   );
 }
 export default ThemeSwitcher;
