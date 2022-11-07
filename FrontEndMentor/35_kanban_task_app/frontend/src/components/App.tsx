88e308828e911867c3398aad5fa0b2e9a13e62be
Simon Stranks <sstranks87@gmail.com>
2022-11-07 12:12:37 +0000

    chore: styles wip

---
+++ FrontEndMentor/35_kanban_task_app/frontend/src/components/App.tsx
@@ -0,0 +1,12 @@
+import Home from '#Pages/Home';
+import { Route, Routes } from 'react-router-dom';
+
+function App(): JSX.Element {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
+
+export default App;
--- FrontEndMentor/35_kanban_task_app/frontend/src/index.tsx
+++ FrontEndMentor/35_kanban_task_app/frontend/src/index.tsx
@@ -1,8 +1,8 @@
+import App from '#Components/App';
+import '#Sass/global-imports.scss';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './assets/sass/global-imports.scss';
-import App from './components/App';
 
 const container = document.querySelector('#root');
 // eslint-disable-next-line  @typescript-eslint/no-non-null-assertion
