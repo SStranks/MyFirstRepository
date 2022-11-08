edec46786f6d11ad18da1875a8a467926508133a
Simon Stranks <sstranks87@gmail.com>
2022-11-08 11:36:56 +0000

    chore: styles wip

--- FrontEndMentor/35_kanban_task_app/frontend/src/components/aside/Aside.tsx
+++ FrontEndMentor/35_kanban_task_app/frontend/src/components/aside/Aside.tsx
@@ -3,12 +3,20 @@ import IconBoard from '#Svg/icon-board.svg';
 import IconDarkTheme from '#Svg/icon-dark-theme.svg';
 import IconHideSidebar from '#Svg/icon-hide-sidebar.svg';
 import IconLightTheme from '#Svg/icon-light-theme.svg';
+import IconShowSidebar from '#Svg/icon-show-sidebar.svg';
 // import IconShowSidebar from '#Svg/icon-show-sidebar.svg';
 import styles from './_Aside.module.scss';
 
-const sidebarToggle = () => {
+const sidebarHide = () => {
   const sidebarElement = document.querySelector('#sidebar') as HTMLElement;
-  sidebarElement.classList.add(styles.animation);
+  sidebarElement.classList.add(styles['animation-hide']);
+  sidebarElement.classList.remove(styles['animation-show']);
+};
+
+const sidebarShow = () => {
+  const sidebarElement = document.querySelector('#sidebar') as HTMLElement;
+  sidebarElement.classList.add(styles['animation-show']);
+  // sidebarElement.classList.remove(styles['animation-hide']);
 };
 
 function Aside(): JSX.Element {
@@ -44,11 +52,18 @@ function Aside(): JSX.Element {
         <button
           type="button"
           className={styles.sidebar__hide}
-          onClick={sidebarToggle}>
+          onClick={sidebarHide}>
           <img src={IconHideSidebar} alt="" />
           <p>Hide Sidebar</p>
         </button>
       </div>
+      <button
+        type="button"
+        className={styles.sidebar__show}
+        onClick={sidebarShow}
+        id="sidebar-btn-show">
+        <img src={IconShowSidebar} alt="" />
+      </button>
     </aside>
   );
 }
