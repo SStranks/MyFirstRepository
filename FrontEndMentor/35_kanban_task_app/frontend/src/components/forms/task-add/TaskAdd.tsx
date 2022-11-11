9f152d6fff2e9616f96730222452d282b0b3b10c
Simon Stranks <sstranks87@gmail.com>
2022-11-11 12:56:31 +0000

    chore: styles wip

--- FrontEndMentor/35_kanban_task_app/frontend/src/components/forms/task-add/TaskAdd.tsx
+++ FrontEndMentor/35_kanban_task_app/frontend/src/components/forms/task-add/TaskAdd.tsx
@@ -1,5 +1,5 @@
 import Dropdown from '#Components/custom/dropdown/Dropdown';
-import IconCross from '#Svg/icon-cross.svg';
+import InputTextSubtask from '#Components/custom/input-text/InputTextSubtask';
 import styles from './_TaskAdd.module.scss';
 
 const submitHandler = () => {
@@ -13,23 +13,24 @@ function TaskAdd(): JSX.Element {
         <p className={styles.form__title}>Add New Task</p>
         <div className={styles.form__group}>
           <p>Title</p>
-          <input type="text" placeholder="e.g. Take coffee break" />
+          <input
+            type="text"
+            placeholder="e.g. Take coffee break"
+            className={styles.form__input}
+          />
         </div>
         <div className={styles.form__group}>
           <p>Description</p>
-          <textarea placeholder="It's always good to take a break. This 15 minute break will recharge the batteries a little" />
+          <textarea
+            className={`${styles.form__input} ${styles.form__textarea}`}
+            placeholder="It's always good to take a break. This 15 minute break will recharge the batteries a little"
+          />
         </div>
         <div className={styles.form__group}>
           <p>Sub-Tasks</p>
           <div className={styles['form__sub-tasks']}>
-            <div className={styles['form__sub-tasks__input']}>
-              <input type="text" placeholder="e.g. Make coffee" />
-              <img src={IconCross} alt="" />
-            </div>
-            <div className={styles['form__sub-tasks__input']}>
-              <input type="text" placeholder="e.g. Drink coffee and smile" />
-              <img src={IconCross} alt="" />
-            </div>
+            <InputTextSubtask />
+            <InputTextSubtask />
           </div>
           <button type="button" className={styles['form__btn-new-sub-task']}>
             + Add New Sub-Task
