08020f68a6f17a322b6454b7b4530dc8e18975dd
Simon Stranks <sstranks87@gmail.com>
2022-11-12 22:23:50 +0000

    chore: wip

--- FrontEndMentor/35_kanban_task_app/frontend/src/components/forms/task-add/TaskAdd.tsx
+++ FrontEndMentor/35_kanban_task_app/frontend/src/components/forms/task-add/TaskAdd.tsx
@@ -18,13 +18,13 @@ function TaskAdd(): JSX.Element {
     console.log(inputData);
 
     // Check if any of the inputs are empty; set component global error
-    if (Object.entries(inputData).some((input) => input[1] === '')) {
-      setFormError(true);
-      // eslint-disable-next-line no-return-assign
-      setSubTasks((prev) =>
-        [...prev].map((el) => (el.props?.formError = true))
-      );
-    }
+    // if (Object.entries(inputData).some((input) => input[1] === '')) {
+    //   setFormError(true);
+    //   // eslint-disable-next-line no-return-assign
+    //   setSubTasks((prev) =>
+    //     [...prev].map((el) => (el.props?.formError = true))
+    //   );
+    // }
   };
 
   const deleteFn = (listId: number): void => {
