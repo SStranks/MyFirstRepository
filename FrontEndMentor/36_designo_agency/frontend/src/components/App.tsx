e0bf952756cb4449e3b76b0a28f019092c7134ae
Simon Stranks <sstranks87@gmail.com>
2023-02-28 22:52:43 +0000

    styles(various): initial draft

--- FrontEndMentor/36_designo_agency/frontend/src/components/App.tsx
+++ FrontEndMentor/36_designo_agency/frontend/src/components/App.tsx
@@ -1,6 +1,7 @@
 import Home from '#Pages/Home';
 import { Route, Routes } from 'react-router-dom';
 
+// TODO:  Sort out background image in CSS for footer hero
 // TODO:  Updates all img src attributes
 
 function App(): JSX.Element {
--- FrontEndMentor/36_designo_agency/frontend/src/components/footer/Footer.tsx
+++ FrontEndMentor/36_designo_agency/frontend/src/components/footer/Footer.tsx
@@ -9,36 +9,52 @@ import styles from './_Footer.module.scss';
 function Footer(): JSX.Element {
   return (
     <div className={styles.footer}>
-      <div className={styles.footer__nav}>
-        <img className={styles.footer__logo} src={LogoLight} alt="" />
-        <div className={styles.footer__links}>
-          <p className={styles.footer__link}>our company</p>
-          <p className={styles.footer__link}>locations</p>
-          <p className={styles.footer__link}>contact</p>
-        </div>
-      </div>
-      <hr className={styles.footer__hr} />
-      <div className={styles.footer__info}>
-        <div className={styles.footer__details}>
+      <div className={styles.footer__container}>
+        <div className={styles.footer__hero}>
           <div>
-            <p className={styles['footer__bold-text']}>
-              Designo Central Office
+            <p className={styles.footer__hero__header}>
+              Let&#39;s talk about your project
             </p>
-            <p>3386 Wellington Street</p>
-            <p>Toronto, Ontario M9C 3J5</p>
+            <p className={styles.footer__hero__body}>
+              Ready to take it to the next level? Contact us today and find out
+              how our expertise can help your business grow.
+            </p>
           </div>
-          <div className={styles['footer__bold-text']}>
-            <p>Contact Us &#40;Central Office&#41;</p>
-            <p>P : +1 253-863-8967</p>
-            <p>M : contact@designo.co</p>
+          <button className={styles.footer__hero__button} type="button">
+            get in touch
+          </button>
+        </div>
+        <div className={styles.footer__nav}>
+          <img className={styles.footer__logo} src={LogoLight} alt="" />
+          <div className={styles.footer__links}>
+            <p className={styles.footer__link}>our company</p>
+            <p className={styles.footer__link}>locations</p>
+            <p className={styles.footer__link}>contact</p>
           </div>
         </div>
-        <div className={styles.footer__icons}>
-          <img src={IconFacebook} alt="" />
-          <img src={IconYoutube} alt="" />
-          <img src={IconTwitter} alt="" />
-          <img src={IconPinterest} alt="" />
-          <img src={IconInstagram} alt="" />
+        <hr className={styles.footer__hr} />
+        <div className={styles.footer__info}>
+          <div className={styles.footer__details}>
+            <div>
+              <p className={styles['footer__bold-text']}>
+                Designo Central Office
+              </p>
+              <p>3386 Wellington Street</p>
+              <p>Toronto, Ontario M9C 3J5</p>
+            </div>
+            <div className={styles['footer__bold-text']}>
+              <p>Contact Us &#40;Central Office&#41;</p>
+              <p>P : +1 253-863-8967</p>
+              <p>M : contact@designo.co</p>
+            </div>
+          </div>
+          <div className={styles.footer__icons}>
+            <img src={IconFacebook} alt="" />
+            <img src={IconYoutube} alt="" />
+            <img src={IconTwitter} alt="" />
+            <img src={IconPinterest} alt="" />
+            <img src={IconInstagram} alt="" />
+          </div>
         </div>
       </div>
     </div>
