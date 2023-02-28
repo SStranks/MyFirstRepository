b15f3e5b7756a5809c757e726a919dbd44d2f9af
Simon Stranks <sstranks87@gmail.com>
2023-02-28 15:51:34 +0000

    init(project): initialization

---
+++ FrontEndMentor/36_designo_agency/frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from './_Footer.module.scss';
+
+function Footer(): JSX.Element {
+  return (
+    <div className={styles.footer}>
+      <div className="">Test</div>
+    </div>
+  );
+}
+
+export default Footer;
