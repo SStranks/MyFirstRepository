a98a016ee61af92d17d599816655c34a5b395536
Simon Stranks <sstranks87@gmail.com>
2023-03-01 18:05:41 +0000

    styles(home page): wip

--- FrontEndMentor/36_designo_agency/frontend/src/pages/Home.tsx
+++ FrontEndMentor/36_designo_agency/frontend/src/pages/Home.tsx
@@ -1,6 +1,11 @@
+import Card from '#Components/card/Card';
 import Nav from '#Components/nav/Nav';
-import ImgHero from '#Img/desktop/image-hero-phone.png';
 import DefaultLayout from '#Layouts/DefaultLayout';
+
+import ImgAppDesign from '#Img/desktop/image-app-design.jpg';
+import ImgGraphicDesign from '#Img/desktop/image-graphic-design.jpg';
+import ImgHero from '#Img/desktop/image-hero-phone.png';
+import ImgWebDesign from '#Img/desktop/image-web-design-large.jpg';
 import styles from './_Home.module.scss';
 
 function Home(): JSX.Element {
@@ -18,6 +23,11 @@ function Home(): JSX.Element {
           <button type="button">learn more</button>
         </div>
         <img src={ImgHero} alt="" />
+      </div>
+      <div className={styles.viewgrid}>
+        <Card title="web design" image={ImgWebDesign} />
+        <Card title="app design" image={ImgAppDesign} />
+        <Card title="graphic design" image={ImgGraphicDesign} />
       </div>
     </DefaultLayout>
   );
