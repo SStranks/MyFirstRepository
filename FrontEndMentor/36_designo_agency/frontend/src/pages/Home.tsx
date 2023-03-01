81a7dc89434141283508c0740ff0a60042740f68
Simon Stranks <sstranks87@gmail.com>
2023-03-01 19:05:31 +0000

    chore(home page): wip

--- FrontEndMentor/36_designo_agency/frontend/src/pages/Home.tsx
+++ FrontEndMentor/36_designo_agency/frontend/src/pages/Home.tsx
@@ -1,11 +1,16 @@
 import Card from '#Components/card/Card';
 import Nav from '#Components/nav/Nav';
+import Quality from '#Components/quality/Quality';
 import DefaultLayout from '#Layouts/DefaultLayout';
 
 import ImgAppDesign from '#Img/desktop/image-app-design.jpg';
 import ImgGraphicDesign from '#Img/desktop/image-graphic-design.jpg';
 import ImgHero from '#Img/desktop/image-hero-phone.png';
 import ImgWebDesign from '#Img/desktop/image-web-design-large.jpg';
+import SvgFriendly from '#Svg/desktop/illustration-friendly.svg';
+import SvgPassionate from '#Svg/desktop/illustration-passionate.svg';
+import SvgResourceful from '#Svg/desktop/illustration-resourceful.svg';
+
 import styles from './_Home.module.scss';
 
 function Home(): JSX.Element {
@@ -28,6 +33,23 @@ function Home(): JSX.Element {
         <Card title="web design" image={ImgWebDesign} />
         <Card title="app design" image={ImgAppDesign} />
         <Card title="graphic design" image={ImgGraphicDesign} />
+      </div>
+      <div className="">
+        <Quality
+          title="passionate"
+          caption="Each project starts with an in-depth brand research to ensure we only create products that serve a purpose. We merge art, design, and technology into exciting new solutions."
+          illustration={SvgPassionate}
+        />
+        <Quality
+          title="resourceful"
+          caption="Everything that we do has a strategic purpose. We use an agile approach in all of our projects and value customer collaboration. It guarantees superior results that fulfill our clients’ needs."
+          illustration={SvgResourceful}
+        />
+        <Quality
+          title="friendly"
+          caption="We are a group of enthusiastic folks who know how to put people first. Our success depends on our customers, and we strive to give them the best experience a company can provide."
+          illustration={SvgFriendly}
+        />
       </div>
     </DefaultLayout>
   );
