612cdac8ac3f105507ceb8d3d22bffdb8572c415
Simon Stranks <sstranks87@gmail.com>
2023-03-04 22:47:37 +0000

    styles(locations): first draft

--- FrontEndMentor/36_designo_agency/frontend/src/components/App.tsx
+++ FrontEndMentor/36_designo_agency/frontend/src/components/App.tsx
@@ -1,7 +1,9 @@
 import About from '#Pages/about/About';
 import AppDesign from '#Pages/app-design/AppDesign';
+import Contact from '#Pages/contact/Contact';
 import GraphicDesign from '#Pages/graphic-design/GraphicDesign';
 import Home from '#Pages/home/Home';
+import Locations from '#Pages/locations/Locations';
 import WebDesign from '#Pages/web-design/WebDesign';
 import { Route, Routes } from 'react-router-dom';
 
@@ -19,7 +21,9 @@ function App(): JSX.Element {
       <Route path="/webdesign" element={<WebDesign />} />
       <Route path="/appdesign" element={<AppDesign />} />
       <Route path="/graphicdesign" element={<GraphicDesign />} />
+      <Route path="/locations" element={<Locations />} />
       <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
   );
 }
