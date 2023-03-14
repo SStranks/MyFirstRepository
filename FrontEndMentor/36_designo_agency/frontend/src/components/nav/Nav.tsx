8189f194c1bc98e55497f2c9254db8e22ed2c83f
Simon Stranks <sstranks87@gmail.com>
2023-03-14 18:03:07 +0000

    feat(nav): mobile modal menu

--- FrontEndMentor/36_designo_agency/frontend/src/components/nav/Nav.tsx
+++ FrontEndMentor/36_designo_agency/frontend/src/components/nav/Nav.tsx
@@ -1,17 +1,27 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import LogoDark from '#Img/desktop/logo-dark.png';
+import IconClose from '#Svg/mobile/icon-close.svg';
 import IconMenu from '#Svg/mobile/icon-hamburger.svg';
 import styles from './_Nav.module.scss';
 
 function Nav(): JSX.Element {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const mobileMenuBtn = () => {
+    setMobileMenuOpen((prev) => !prev);
+  };
+
   return (
     <div className={styles.nav}>
       <Link to="/">
         <img src={LogoDark} alt="" className={styles.nav__logo} />
       </Link>
-      <img src={IconMenu} alt="" />
-      <div className={styles.nav__links}>
+      <button type="button" onClick={mobileMenuBtn}>
+        <img src={mobileMenuOpen ? IconClose : IconMenu} alt="" />
+      </button>
+      <div className={`${styles.nav__links} ${mobileMenuOpen ? '' : 'hidden'}`}>
         <Link to="/about">
           <p className={styles.nav__link}>our company</p>
         </Link>
