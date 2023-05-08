677f8eab69140dc4a9b4008ef23b91ff940670eb
Simon Stranks <sstranks87@gmail.com>
2023-05-08 16:30:50 +0100

    init()

---
+++ FrontEndMentor/37_audiophile_ecommerce/frontend/src/components/custom/SkipTo.tsx
@@ -0,0 +1,29 @@
+// import { Link } from 'react-router-dom';
+import styles from './_SkipTo.module.scss';
+
+type ElemProps = {
+  contentName: string;
+  contentId: string;
+};
+
+// Accessibility; hidden by default, allows users to skip page content
+function SkipTo(props: ElemProps): JSX.Element {
+  const { contentName, contentId } = props;
+
+  // NOTE:  React-Router can't handle #links. Manual focus on #HTMLElement.
+  const onClickHandler = (e: React.MouseEvent) => {
+    e.preventDefault();
+    const element = document.querySelector(contentId) as HTMLElement;
+    element?.setAttribute('tabIndex', '0');
+    element?.focus();
+    element?.removeAttribute('tabIndex');
+  };
+
+  return (
+    <a className={styles.container} href={contentId} onClick={onClickHandler}>
+      Skip to {contentName}
+    </a>
+  );
+}
+
+export default SkipTo;
