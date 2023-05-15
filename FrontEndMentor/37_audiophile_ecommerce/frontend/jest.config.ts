84f5c8f454647168e086e7dc39c8c72154cad1f0
Simon Stranks <sstranks87@gmail.com>
2023-05-15 15:24:00 +0100

    fix(): updated modulenamemapper

--- FrontEndMentor/37_audiophile_ecommerce/frontend/jest.config.ts
+++ FrontEndMentor/37_audiophile_ecommerce/frontend/jest.config.ts
@@ -18,6 +18,20 @@ const config: Config = {
     // '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
     //   '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '^#Img/(.*)$': '<rootDir>/src/assets/img/$1',
+    '^#Sass/(.*)$': '<rootDir>/src/assets/sass/$1',
+    '^#Svg/(.*)$': '<rootDir>/src/assets/svg/$1',
+    '^#Components/(.*)$': '<rootDir>/src/components/$1',
+    '^#Context/(.*)$': '<rootDir>/src/context/$1',
+    '^#Data/(.*)$': '<rootDir>/src/data/$1',
+    '^#Features/(.*)$': '<rootDir>/src/features/$1',
+    '^#Hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^#Layouts/(.*)$': '<rootDir>/src/layouts/$1',
+    '^#Libs/(.*)$': '<rootDir>/src/libs/$1',
+    '^#Pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^#Services/(.*)$': '<rootDir>/src/services/$1',
+    '^#Types/(.*)$': '<rootDir>/src/types/$1',
+    '^#Utils/(.*)$': '<rootDir>/src/utils/$1',
   },
 };
 
