3f491b1943265dc02bd1c3e2321dbc0ea43437e4
Simon Stranks <sstranks87@gmail.com>
2023-05-19 14:56:48 +0100

    test(various): wip

--- FrontEndMentor/37_audiophile_ecommerce/frontend/src/components/checkout/CartProductCard.test.tsx
+++ FrontEndMentor/37_audiophile_ecommerce/frontend/src/components/checkout/CartProductCard.test.tsx
@@ -1,6 +1,4 @@
-import { ShoppingCartProvider } from '#Context/ShoppingCartContext';
 import { render, screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
 import CartProductCard from './CartProductCard';
 
 const mockQuantityToggleButton = jest.fn();
@@ -46,44 +44,4 @@ describe('Appearance', () => {
       })
     );
   });
-});
-
-describe('Functionality', () => {
-  // eslint-disable-next-line jest/no-commented-out-tests
-  // test('Button click amends context cartItem[]; increases product (id) by specified quantity; from null to 1 quantity', async () => {
-  //   // Dummy JSX to render out cartItems[] from context
-  //   const TestContextCartTotal = (): JSX.Element => {
-  //     const { cartItems } = useShoppingCartContext();
-  //     const items = cartItems.map(({ id, quantity }) => {
-  //       return (
-  //         <p key={id} data-testid="cartItem">
-  //           id: {id} || quantity: {quantity}
-  //         </p>
-  //       );
-  //     });
-  //     return <div data-testid="cartItems">{items}</div>;
-  //   };
-  //   render(
-  //     <ShoppingCartProvider>
-  //       <TestContextCartTotal />
-  //       <CartProductCard
-  //         productId={1}
-  //         productImg="productImage"
-  //         productTitle="productTitle"
-  //         productPrice={123}
-  //         productQuantity={66}
-  //       />
-  //     </ShoppingCartProvider>
-  //   );
-  //   const testContextCartTotal = screen.getByTestId('cartItems');
-  //   const button = screen.getByRole('button');
-  //   expect(testContextCartTotal).toBeEmptyDOMElement();
-  //   await userEvent.click(button);
-  //   expect(testContextCartTotal).toContainElement(
-  //     screen.getByTestId('cartItem')
-  //   );
-  //   expect(screen.getByTestId('cartItem')).toHaveTextContent(
-  //     'id: 1 || quantity: 1'
-  //   );
-  // });
 });
---
+++ FrontEndMentor/37_audiophile_ecommerce/frontend/src/components/checkout/CartSummaryCard.test.tsx
@@ -0,0 +1,106 @@
+import { ShoppingCartProvider } from '#Context/ShoppingCartContext';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
+import { BrowserRouter, Router } from 'react-router-dom';
+import CartSummaryCard from './CartSummaryCard';
+
+const mockedUsedNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedUsedNavigate,
+}));
+
+beforeEach(() => {
+  mockedUsedNavigate.mockReset();
+});
+
+describe('Appearance', () => {
+  test('Component base should be fully rendered', () => {
+    const mockFn = jest.fn();
+    const { container } = render(
+      <ShoppingCartProvider>
+        <CartSummaryCard closeCartModal={mockFn} />
+      </ShoppingCartProvider>,
+      {
+        wrapper: BrowserRouter,
+      }
+    );
+
+    const component = container.querySelector('div');
+    const cartItemsQuantity = screen.getByText(/^cart \(\d\)$/);
+    const removeAllBtn = screen.getByRole('button', {
+      name: 'remove all products from cart',
+    });
+    const cartItemsTotal = screen.getByText(/^\$ \d+\.\d{2}$/);
+    const checkoutBtn = screen.getByRole('button', { name: 'checkout' });
+
+    expect(component).toBeInTheDocument();
+    expect(cartItemsQuantity).toBeInTheDocument();
+    expect(removeAllBtn).toBeInTheDocument();
+    expect(cartItemsTotal).toBeInTheDocument();
+    expect(checkoutBtn).toBeInTheDocument();
+  });
+
+  test('If already on checkout route do not render goto checkout button', () => {
+    const mockFn = jest.fn();
+    const history = createMemoryHistory({ initialEntries: ['/checkout'] });
+    history.push = jest.fn();
+
+    render(
+      <Router location={history.location} navigator={history}>
+        <ShoppingCartProvider>
+          <CartSummaryCard closeCartModal={mockFn} />
+        </ShoppingCartProvider>
+      </Router>
+    );
+
+    expect(
+      screen.queryByRole('button', { name: 'checkout' })
+    ).not.toBeInTheDocument();
+  });
+});
+
+describe('Functionality', () => {
+  // eslint-disable-next-line jest/no-commented-out-tests
+  test('If cart is empty checkout button should be disabled', () => {
+    // NOTE:  By default cart is empty as test is not accessing local storage in cart context.
+    const mockFn = jest.fn();
+    render(
+      <ShoppingCartProvider>
+        <CartSummaryCard closeCartModal={mockFn} />
+      </ShoppingCartProvider>,
+      {
+        wrapper: BrowserRouter,
+      }
+    );
+
+    const checkoutBtn = screen.getByRole('button', { name: 'checkout' });
+
+    expect(checkoutBtn).toBeDisabled();
+  });
+
+  test('Clicking checkout button navigates to checkout route and closes modal', async () => {
+    const mockFn = jest.fn();
+    localStorage.setItem(
+      'shopping-cart',
+      JSON.stringify([{ id: 1, quantity: 1 }])
+    );
+
+    render(
+      <ShoppingCartProvider>
+        <CartSummaryCard closeCartModal={mockFn} />
+      </ShoppingCartProvider>,
+      { wrapper: BrowserRouter }
+    );
+
+    const checkoutBtn = screen.getByRole('button', { name: 'checkout' });
+
+    await userEvent.click(checkoutBtn);
+    expect(mockFn).toHaveBeenCalledTimes(1);
+    await waitFor(() => expect(mockedUsedNavigate).toHaveBeenCalledTimes(1));
+    await waitFor(() =>
+      expect(mockedUsedNavigate).toHaveBeenCalledWith('/checkout')
+    );
+  });
+});
