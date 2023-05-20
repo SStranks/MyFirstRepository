c298ccef76d3c0ca2e4f7f33ec6ead63ff49e8e8
Simon Stranks <sstranks87@gmail.com>
2023-05-20 10:26:13 +0100

    test(): complete

--- FrontEndMentor/37_audiophile_ecommerce/frontend/src/components/checkout/OrderCompleteCard.test.tsx
+++ FrontEndMentor/37_audiophile_ecommerce/frontend/src/components/checkout/OrderCompleteCard.test.tsx
@@ -1,6 +1,6 @@
-import ViewMoreCartItemsButton from '#Components/custom/buttons/ViewMoreCartItemsButton';
 import { ShoppingCartProvider } from '#Context/ShoppingCartContext';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import OrderCompleteCard from './OrderCompleteCard';
 
 describe('Appearance', () => {
@@ -8,11 +8,9 @@ describe('Appearance', () => {
     const mockFn = jest.fn();
     localStorage.setItem(
       'shopping-cart',
-      JSON.stringify([
-        { id: 1, quantity: 3 },
-        { id: 2, quantity: 3 },
-      ])
+      JSON.stringify([{ id: 1, quantity: 1 }])
     );
+
     render(
       <ShoppingCartProvider>
         <OrderCompleteCard modalClose={mockFn} />
@@ -27,21 +25,63 @@ describe('Appearance', () => {
     const textInfo = screen.getByText(
       'You will receive an email conformation shortly'
     );
-    const hrSeparator = screen.getByRole('separator');
     const textGrandTotal = screen.getByText('grand total');
     const textTotalAmount = screen.getByText(
       /^\$(0|[1-9]\d{0,2})(,\d{3})*(\.\d{1,2})?$/
     );
     const backToHomeBtn = screen.getByRole('button', { name: 'back to home' });
+    const hrSeparator = screen.queryByRole('separator');
+    const btns = screen.queryAllByRole('button');
 
     expect(component).toBeInTheDocument();
     expect(iconCheck).toBeInTheDocument();
     expect(textH3).toBeInTheDocument();
     expect(textInfo).toBeInTheDocument();
-    expect(hrSeparator).toBeInTheDocument();
     expect(textGrandTotal).toBeInTheDocument();
     expect(textTotalAmount).toBeInTheDocument();
     expect(backToHomeBtn).toBeInTheDocument();
-    expect(component.contains(<ViewMoreCartItemsButton />)).toBe(true);
+    expect(hrSeparator).not.toBeInTheDocument();
+    // eslint-disable-next-line jest-dom/prefer-in-document
+    expect(btns).toHaveLength(1);
+  });
+
+  test('When cart has more than one type of item, hr and `view more` button should be visible', () => {
+    const mockFn = jest.fn();
+    localStorage.setItem(
+      'shopping-cart',
+      JSON.stringify([
+        { id: 1, quantity: 3 },
+        { id: 2, quantity: 3 },
+      ])
+    );
+
+    render(
+      <ShoppingCartProvider>
+        <OrderCompleteCard modalClose={mockFn} />
+      </ShoppingCartProvider>
+    );
+
+    const hrSeparator = screen.getByRole('separator');
+    const btns = screen.getAllByRole('button');
+
+    expect(hrSeparator).toBeInTheDocument();
+    expect(btns).toHaveLength(2);
+  });
+});
+
+describe('Functionality', () => {
+  test('Clicking `back to home` button fires closes modal', async () => {
+    const mockFn = jest.fn();
+
+    render(
+      <ShoppingCartProvider>
+        <OrderCompleteCard modalClose={mockFn} />
+      </ShoppingCartProvider>
+    );
+
+    const backBtn = screen.getByRole('button', { name: 'back to home' });
+
+    await userEvent.click(backBtn);
+    expect(mockFn).toHaveBeenCalledTimes(1);
   });
 });
