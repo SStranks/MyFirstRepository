6684f801762577f4e32f40f18b53dc051c7662a2
Simon Stranks <sstranks87@gmail.com>
2023-05-26 13:30:45 +0100

    fix(app files): extracted 'todos' into .md file

--- FrontEndMentor/36_designo_agency/frontend/src/components/App.tsx
+++ FrontEndMentor/36_designo_agency/frontend/src/components/App.tsx
@@ -7,24 +7,6 @@ import Locations from '#Pages/locations/Locations';
 import WebDesign from '#Pages/web-design/WebDesign';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
-// TODO:  Add 'skip-to' functionality - see proj#37
-// TODO:  Sort out circles on backgrounds on mobile sized layout
-// TODO:  Add all z-index to control manager
-// TODO:  Complete tasks on readme: locations API. form error status.
-// TODO:  // ✔  Sort form functionality and hover affecting vertical alignment of elements
-// TODO:  // ✔  Updates all img src attributes
-// TODO:  // ✔  Check all resizing/overflow on blocks
-// TODO:  // ✔  Fix the images on locations page (zoom and crop for smaller screen sizes)
-// TODO:  // ✔  Sort out hero mobile image
-// TODO:  // ✔  Sort out mobile hamburger menu
-// TODO:  // ✔  Sort out 'qualities' - has different grid layout on tablet home vs about pages
-// TODO:  // ✔  Solve aliasing for CSS
-// ✔
-// ✖
-
-// NOTE:  Could refactor some of the SASS for shared hero elements and layout potential.
-// NOTE:  Could refactor negative margin on about page / make variable to hold -24px for use elsewhere
-
 function App(): JSX.Element {
   return (
     <Routes>
