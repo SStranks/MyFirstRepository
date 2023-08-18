a90df4848f2923d7c344b1763271f087c04bbb52
Simon Stranks <sstranks87@gmail.com>
2023-08-18 17:00:33 +0100

    feat(input price): complete functionality

--- FrontEndMentor/34_invoice_app/frontend/src/components/modal_v2/FormItem.tsx
+++ FrontEndMentor/34_invoice_app/frontend/src/components/modal_v2/FormItem.tsx
@@ -2,6 +2,19 @@
 import IconDelete from '#Svg/icon-delete.svg';
 import { Fragment, useEffect, useRef, useState } from 'react';
 import styles from './FormItem.module.scss';
+import InputPrice from './InputPrice';
+
+const numberFormatter = new Intl.NumberFormat('en-US', {
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+  useGrouping: false,
+});
+
+const currencyFormatter = new Intl.NumberFormat('en-US', {
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+  useGrouping: true,
+});
 
 interface IProps {
   id?: string;
@@ -28,7 +41,6 @@ function FormItem(props: IProps): JSX.Element {
   );
   const [total, setTotal] = useState<number>(totalProp ?? 0);
   const quantityRef = useRef<HTMLInputElement>(null);
-  const priceRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     if (quantity !== undefined && price !== undefined)
@@ -66,56 +78,6 @@ function FormItem(props: IProps): JSX.Element {
     return null;
   };
 
-  const numFormatter = new Intl.NumberFormat('en-US', {
-    minimumFractionDigits: 2,
-  });
-
-  const priceOnKeyDown = (e: React.KeyboardEvent) => {
-    switch (true) {
-      case e.key === 'Backspace':
-      case e.key === 'Tab':
-      case e.key === 'ArrowUp':
-      case e.key === 'ArrowDown':
-      case e.key === 'ArrowLeft':
-      case e.key === 'ArrowRight':
-        return null;
-      case e.key === '-':
-      case e.key === '+':
-      case e.key === 'e':
-        return e.preventDefault();
-      case price === 0 && e.key === '0':
-        return e.preventDefault();
-      case price !== undefined &&
-        !Number.isNaN(Number(e.key)) &&
-        /\d+\.\d\d/.test(price.toString()) &&
-        /\d+\.\d\d/.test(numFormatter.format(Number(price))):
-        return e.preventDefault();
-      default:
-        return null;
-    }
-  };
-
-  // eslint-disable-next-line consistent-return
-  const priceOnChange = (e: React.ChangeEvent) => {
-    const { value } = e.target as HTMLInputElement;
-    if (value === '') return setPrice(undefined);
-    setPrice(value);
-  };
-
-  // eslint-disable-next-line unicorn/consistent-function-scoping
-  const priceOnBlur = () => {
-    if (!price) setPrice(0);
-    if (priceRef.current) {
-      const { value } = priceRef.current;
-      priceRef.current.value = numFormatter.format(Number(value));
-    }
-    return null;
-  };
-
-  const priceOnFocus = () => {
-    if (priceRef.current?.value === '0.00') setPrice(undefined);
-  };
-
   // eslint-disable-next-line unicorn/consistent-function-scoping
   const deleteOnClick = (e: React.MouseEvent) => {
     console.log('delete', e);
@@ -143,21 +105,13 @@ function FormItem(props: IProps): JSX.Element {
         onBlur={quantityOnBlur}
         required
       />
-      <input
-        className={`${styles.price} ${styles.input}`}
-        type="number"
-        ref={priceRef}
-        step={0.01}
-        pattern="^\d*(\.\d{0,2})?$"
-        min={0}
-        value={price ?? ''}
-        onKeyDown={priceOnKeyDown}
-        onChange={priceOnChange}
-        onBlur={priceOnBlur}
-        onFocus={priceOnFocus}
+      <InputPrice
+        price={price}
+        setPrice={setPrice}
+        currencyFormatter={numberFormatter}
         required
       />
-      <p className={styles.total}>{numFormatter.format(total)}</p>
+      <p className={styles.total}>{currencyFormatter.format(total)}</p>
       <button type="button" onClick={deleteOnClick}>
         <img className={styles.deleteBtn} src={IconDelete} alt="" />
       </button>
---
+++ FrontEndMentor/34_invoice_app/frontend/src/components/modal_v2/InputPrice.tsx
@@ -0,0 +1,89 @@
+import { useMemo, useState } from 'react';
+import styles from './FormItem.module.scss';
+
+interface IProps {
+  price?: string | number | undefined;
+  setPrice?: React.Dispatch<React.SetStateAction<number | string | undefined>>;
+  currencyFormatter?: Intl.NumberFormat;
+  required: boolean;
+}
+
+function InputPrice(props: IProps): JSX.Element {
+  const {
+    price: priceProp,
+    setPrice: setPriceProp,
+    currencyFormatter: currencyFormatterProp,
+    required,
+  } = props;
+  const [priceInternal, setPriceInternal] = useState<
+    number | string | undefined
+  >(0);
+
+  // If price is managed by parent component, use that components state setter function.
+  const setPrice = (value: number | string | undefined) => {
+    if (setPriceProp) return setPriceProp(value);
+    return setPriceInternal(value);
+  };
+
+  // If price is managed by parent component, use that components state value.
+  const price = priceProp === undefined ? priceInternal : priceProp;
+
+  const currencyFormatter = useMemo(() => {
+    if (currencyFormatterProp === undefined)
+      return new Intl.NumberFormat('en-US', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+        useGrouping: false,
+      });
+    return currencyFormatterProp;
+  }, [currencyFormatterProp]);
+
+  const priceOnKeyDown = (e: React.KeyboardEvent) => {
+    switch (true) {
+      case e.key === 'Backspace':
+      case e.key === 'Tab':
+      case e.key === 'ArrowUp':
+      case e.key === 'ArrowDown':
+      case e.key === 'ArrowLeft':
+      case e.key === 'ArrowRight':
+        return null;
+      case e.key === '-':
+      case e.key === '+':
+      case e.key === 'e':
+        return e.preventDefault();
+      case price === 0 && e.key === '0':
+        return e.preventDefault();
+      default:
+        return null;
+    }
+  };
+
+  const priceOnChange = (e: React.ChangeEvent) => {
+    // Constrain input to two decimal places
+    let { value } = e.target as HTMLInputElement;
+    if (/\d+\.\d{3}/.test(value)) value = value.slice(0, -1);
+    return setPrice(value);
+  };
+
+  const priceOnBlur = () => {
+    const formattedValue = currencyFormatter.format(Number(price ?? 0));
+    return setPrice(formattedValue);
+  };
+
+  return (
+    <input
+      className={`${styles.price} ${styles.input}`}
+      type="number"
+      step={0.01}
+      pattern="^\d*(\.\d{0,2})?$"
+      min={0}
+      value={price ?? ''}
+      onKeyDown={priceOnKeyDown}
+      onChange={priceOnChange}
+      onBlur={priceOnBlur}
+      required={required}
+    />
+  );
+}
+
+export default InputPrice;
